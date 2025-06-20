992b511ff93ed8c6f4c3420fe1e447d3ee5b1ae7
CoffeeDrinker <wrrw2006@gmail.com>
2025-06-20 18:11:44 +0300

    Added the possibility to choose what wav file to play.

--- src/main.c
+++ src/main.c
@@ -9,14 +9,17 @@ int16_t temp_samples[SAMPLE_SIZE];
 float input_samples[RECTANGLE_COUNT];
 float smoothed_samples[RECTANGLE_COUNT];
 
-int main()
+int main(int argc, char** argv)
 {
+    if (argc <= 1)
+        return 1;
+
     int init_code = sdl_init();
     if (init_code)
         return init_code;
 
     AudioData ad;
-    int load_code = load_file("/home/coffeedrinker/Repositories/Typeshi/src/faint.wav", &ud, &ad);
+    int load_code = load_file(argv[1], &ud, &ad);
     if (load_code)
         return load_code;
 
@@ -49,6 +52,7 @@ int main()
 
         draw_visual(renderer, smoothed_samples, 1, first_color, second_color);
         SDL_RenderPresent(renderer);
+        SDL_Delay(1);
     }
 
     sdl_quit(&ad, renderer, window);
